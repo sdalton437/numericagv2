b66b3be923b396a08c14976a439dd4dd3461077d
Sam <daltonsam437@gmail.com>
2018-04-08 18:16:56 -0400

    Changed generalviews to reflect change from range of values to single numeric values

--- webapp/views/generalviews.py
+++ webapp/views/generalviews.py
@@ -359,21 +359,21 @@ def submittrails(request):
                 awdr = float(request.POST['awdrother'])
             else:
 
-                lbound, ubound = climate.split('-')
-                awdr = float((int(lbound) + int(ubound)) / 2)
+
+                awdr = float(climate)
 
             if CHU == 'other':
                 chu = float(request.POST['chuother'])
             else:
 
-                lbound, ubound = CHU.split('-')
-                chu = float((int(lbound) + int(ubound)) / 2);
+
+                chu = float(CHU);
 
             if SOM == 'other':
                 som = float(request.POST['somother'])
             else:
-                lbound, ubound = SOM.split('-')
-                som = float((int(lbound) + int(ubound)) / 2);
+
+                som = float(SOM);
 
             print('descrete values ')
             print("chu :", chu)
